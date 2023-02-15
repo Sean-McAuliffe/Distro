9b3efcdd47e85f33f83f21d014cc1962efa634d8
Sean McAuliffe <109864212+Sean-McAuliffe@users.noreply.github.com>
2023-02-15 22:07:51 +0000

    universal edits, untested

--- compute/file_loader/file_loader.py
+++ compute/file_loader/file_loader.py
@@ -1,6 +1,15 @@
 import polars as pl
+import requests
 
 
+"""
+so this class is responsible for finding the datasources we need and 
+returning the df pointer to run a query plan against 
+
+needs: csv, excel, sql, api/http
+
+
+"""
 class FileLoader:
     def __init__(self, datasource:dict) -> pl.LazyFrame:
         self.datasource = datasource
@@ -13,11 +22,18 @@ class FileLoader:
         df = pl.read_sql(query, conn_uri).lazy()
         return df
 
+    def load_api(self, url:str, params:dict, headers:dict, key:str):
+        req = requests.get(url, params=params, headers=headers)
+
+        data = req.json()[key]
+
+        df = pl.read_json(data).lazy()
+        return df
 
     def load_stage(self):
         filetype = self.datasource['type']
         if filetype == 'csv':
             data = self.load_csv(self.datasource['location'], self.datasource['name'])
-
+        
         return data
 
--- compute/prep_handler/prep.py
+++ compute/prep_handler/prep.py
@@ -31,12 +31,30 @@ class Prep_Handler:
         elif op == "!contains":
             return pl.col(col).str.contains(value).is_not()
 
+#df.with_column(pl.col('bar').cast(pl.Int64, strict=False).alias('bar_int'))
+    def handle_data_types(self,df:pl.LazyFrame ,dtype_obj:dict)->pl.LazyFrame:
+        type_dict = {
+            "int": pl.Int64,
+            "text": pl.Utf8,
+            "float": pl.Float64,
+            "bool": pl.Boolean,
+            "date": pl.Date
+        }
+        for x in dtype_obj['columns']:
+            try:
+                df = df.with_column(pl.col(x['column']).cast(x['should_be'], strict=False))
+            except Exception as e:
+                print("ERROR:: {}".format(e))
+                pass
+        return df
     
 
     def prep_stage(self, df:pl.LazyFrame):
         for x in self.config_obj['steps']:
             if x['type'] == 'filter':
                 df = df.filter(self.handle_filter(x['vars']))
+            if x['type'] == 'clean':
+                df = handle_data_types(df, x)
         return df
 
 
