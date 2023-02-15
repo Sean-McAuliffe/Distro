22e59af2147d923f9a7685f649d2ccc8616740d6
Sean McAuliffe <109864212+Sean-McAuliffe@users.noreply.github.com>
2023-02-15 22:22:24 +0000

    try&exxcept blocks in pipeline for app.py, untested bc universal

--- compute/app.py
+++ compute/app.py
@@ -9,6 +9,9 @@ from analysis_handler.analysis import Analysis_Handler
 
 """
 uvicorn app:app --port 5000
+
+
+
 """
 
 app = FastAPI(title='Compute Test', description='this api is attached to the template-engine for compute', version="1.0")
@@ -32,20 +35,52 @@ def handle_export(df, export_obj):
 
 @app.post('/report', tags=["store"])
 async def pipeline(obj:dict=None):
+    meta = {}
     print("INFO:: beginning pipeline")
-    
-    df = FileLoader(obj['datasource'])
-    df = df.load_stage()
+    meta['startTime'] = datetime.now()
 
-    prep = Prep_Handler(df, obj['tasks']['prep'])
-    df = prep.prep_stage(df)
+    try:
+        df = FileLoader(obj['datasource'])
+        df = df.load_stage()
+        meta['loadTime'] = datetime.now()
+    except Exception as e:
+        meta['loadError_time'] = datetime.now()
+        meta['loadError_message'] = str(e)
 
-    analysis = Analysis_Handler(df, obj['tasks']['analysis'])
-    df = analysis.analysis_stage(df)
+    try:
+        prep = Prep_Handler(df, obj['tasks']['prep'])
+        df = prep.prep_stage(df)
+        meta['prepTime'] = datetime.now()
+    except Exception as e:
+        meta['prepError_time'] = datetime.now()
+        meta['prepError_message'] = str(e)
+
+    try:  
+        analysis = Analysis_Handler(df, obj['tasks']['analysis'])
+        df = analysis.analysis_stage(df)
+        meta['analysisTime'] = datetime.now()
+    except Exception as e:
+        meta['analysisError_time'] = datetime.now()
+        meta['analysisError_message'] = str(e)
 
     #fetch for dev, collect(streaming=True) for prod
-    data = df.fetch()
+    try:
+        data = df.fetch()
+        meta['endTime'] = datetime.now()
+    except Exception as e:
+        meta['endTime'] = datetime.now()
+        meta['endTime_message'] = str(e)
 
-    return {data.write_json()}
+    if obj['logging']['enable'] == 'true':
+        ## logic here to send the logging data via http call
+        pass
+    else:
+        del meta
+        pass
+
+    return {"status":"OK",
+            "statusCode": 200,
+            "statusDate": str(datetime.now())
+            "data":data.write_json()}
 
     
